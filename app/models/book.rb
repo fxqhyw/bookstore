75f0435538efe8318ee872b017db45fb2e4e050e
therealloker <therealloker@gmail.com>
2018-07-15 20:39:04 +0300

    add validates to author and books, tests pass

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
+
+  validates :title, :description, :price, :published_at, :height, :width, :depth, presence: true
+  validates :price, :height, :width, :depth, numericality: { greater_than: 0 }
+  validates :published_at, numericality: { less_than_or_equal_to: Time.current.year }
 end
