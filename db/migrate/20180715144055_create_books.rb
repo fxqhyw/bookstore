36837f3d5acdc7fa764915fb8a13a7361e771ccc
therealloker <therealloker@gmail.com>
2018-07-15 17:55:47 +0300

    create book and author models, setup tests env

---
+++ db/migrate/20180715144055_create_books.rb
@@ -0,0 +1,16 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.integer :published_at
+      t.decimal :price, precision: 6, scale: 2
+      t.string :materials
+      t.decimal :height, precision: 4, scale: 1
+      t.decimal :weight, precision: 4, scale: 1
+      t.decimal :depth, precision: 4, scale: 1
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,17 @@
+ActiveRecord::Schema.define(version: 2018_07_15_144055) do
+  enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "published_at"
+    t.decimal "price", precision: 6, scale: 2
+    t.string "materials"
+    t.decimal "height", precision: 4, scale: 1
+    t.decimal "weight", precision: 4, scale: 1
+    t.decimal "depth", precision: 4, scale: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
