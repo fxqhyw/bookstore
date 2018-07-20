a3efec907c8f042a44f0b3769c2e0d1e437fdb70
therealloker <therealloker@gmail.com>
2018-07-21 00:21:05 +0300

    add dynamic title for filters dropdown

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,13 @@
 module BooksHelper
+  TITLES = {
+    newest: 'Newest first',
+    popular: 'Popular first',
+    low_to_high_price: 'Price: Low to high',
+    high_to_low_price: 'Price: High to low',
+    title_a_z: 'A - Z',
+    title_z_a: 'Z - A'
+  }.freeze
+
   def dimension(book)
     "H:#{book.height}\" x W:#{book.width}\"" \
     " x D:#{book.depth}\""
@@ -6,5 +15,9 @@ module BooksHelper
 
   def price_to_euro(price)
     "€#{price}"
+  end
+
+  def filter_title
+    request.GET[:filter] ? TITLES[request.GET[:filter].to_sym] : TITLES[:newest]
   end
 end
