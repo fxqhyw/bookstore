022b01d8255bb72a598947d25e8b7d93bdbeda73
therealloker <therealloker@gmail.com>
2018-07-24 14:27:40 +0300

    add quantity logic to cart, for create order_item, add book to cart from catalog

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,17 @@
 class OrderItemsController < ApplicationController
   def create
-    @order_item = OrderItem.create(order_item_params)
+    @order_item = OrderItem.find_by(book_id: order_item_params[:book_id])
+    if @order_item
+      @order_item.quantity += order_item_params[:quantity].to_i
+      @order_item.save!
+    else
+      @order_item = OrderItem.create(order_item_params)
+    end
+  end
+
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update_attributes(order_item_params)
   end
 
   private
