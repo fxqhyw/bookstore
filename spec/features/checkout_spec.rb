30707a07fdfaddb2d7c505d92fbd05a0d76873e4
therealloker <therealloker@gmail.com>
2018-08-25 00:34:23 +0300

    feature test pass all checkout steps

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -9,12 +9,12 @@ RSpec.feature 'Checkout', type: :feature do
     expect(page).to have_content('Cart is empty')
   end
 
-  describe 'login step', js: true do
+  describe 'login step' do
     before do
-      @book = FactoryBot.create(:book)
-      visit('/catalog')
-      find("#book#{@book.id}_cart_icon").click
-      wait_for_ajax
+      @order_item = FactoryBot.create(:order_item)
+      my_jar = ActionDispatch::Request.new(Rails.application.env_config).cookie_jar
+      my_jar.signed[:order_id] = @order_item.order_id
+      create_cookie(:order_id, my_jar[:order_id])
       visit('/checkouts')
     end
 
@@ -81,6 +81,25 @@ RSpec.feature 'Checkout', type: :feature do
       find('.radio-icon', match: :first).click
       click_button('Save and Continue')
       expect(page).to have_current_path('/checkouts/payment')
+      fill_in 'Card Number', with: '12345678912312'
+      fill_in 'Name on Card', with: 'Loker Lucker'
+      fill_in 'MM / YY', with: '11/22'
+      fill_in 'CVV', with: '123'
+      click_button('Save and Continue')
+      expect(page).to have_current_path('/checkouts/confirm')
+      find('#edit_address_link').click
+      expect(page).to have_current_path('/checkouts/address?edit=true')
+      expect(page).to have_field('Zip', with: address.zip)
+      within '#billing' do
+        fill_in 'Zip', with: '321'
+      end
+      find('.checkbox-icon').click
+      click_button('Save and Continue')
+      expect(page).to have_current_path('/checkouts/confirm')
+      click_on('Place Order')
+      expect(page).to have_content('Thank You for your Order!')
+      click_on('Back to Store')
+      expect(page).to have_current_path('/catalog')
     end
   end
 end
