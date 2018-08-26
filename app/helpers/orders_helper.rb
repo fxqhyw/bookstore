8389ec1a58171f973d1162bfa5af52c3a9e639c3
therealloker <therealloker@gmail.com>
2018-08-26 20:42:33 +0300

    add dynamic orders view

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+  def order_status(status)
+    case status
+    when 'in_queue'
+      'Waiting for processing'
+    when 'in_delivery'
+      'In delivery'
+    when 'delivered'
+      'Delivered'
+    end
+  end
 end
