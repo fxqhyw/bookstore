ce2dc66fd37bf018e002d300410f7c0a3c0d8ef6
therealloker <therealloker@gmail.com>
2018-08-27 15:19:24 +0300

    move order info partial to shared

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,15 +4,15 @@ module OrdersHelper
     when 'waiting_for_proccesing' then I18n.t('orders.waiting')
     when 'in_delivery' then I18n.t('orders.in_delivery')
     when 'delivered' then I18n.t('orders.delivered')
-    else I18n.t('obutton.all')
+    else I18n.t('button.all')
     end
   end
 
   def order_status(status)
     case status
-    when 'in_queue' then 'Waiting for processing'
-    when 'in_delivery' then 'In delivery'
-    when 'delivered' then 'Delivered'
+    when 'in_queue' then I18n.t('orders.waiting')
+    when 'in_delivery' then I18n.t('orders.in_delivery')
+    when 'delivered' then I18n.t('orders.delivered')
     end
   end
 end
