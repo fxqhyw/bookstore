df10d1d78a3e6e3d43b86a56d7bd3ee14f1b68ea
therealloker <therealloker@gmail.com>
2018-08-30 17:34:35 +0300

    admin can see and delete images

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -25,20 +25,47 @@ ActiveAdmin.register Book do
   filter :published_at
   filter :authors, collection: authors
 
-  form do |f|
+  show do
+    default_main_content
+    panel 'images' do
+      book.images.each do |image|
+        span { image_tag url_for(image.variant(resize: '200x300')) }
+      end
+    end
+  end
+
+  form html: { multipart: true } do |f|
     f.inputs do
       f.input :title
       f.input :description
-      f.input :category, as: :select, collection: Category.pluck(:title, :id), include_blank: false
       f.input :published_at
       f.input :quantity
       f.input :materials
       f.input :height
       f.input :width
       f.input :depth
-      f.input :images, as: :file, input_html: { multiple: true }
+      f.input :category, as: :select, collection: Category.pluck(:title, :id), include_blank: false
+    end
+
+    f.inputs 'Images' do
+      if book.images.any?
+        book.images.each do |image|
+          span { image_tag url_for(image.variant(resize: '200x300')) }
+          span { link_to 'Delete', delete_image_admin_book_path(image), book_id: book.id, method: :delete }
+        end
+      end
+      f.input :images, as: :file, input_html: { multiple: true }, allow_destroy: true
+    end
+
+    f.inputs 'Authors' do
       f.input :author_ids, as: :check_boxes, collection: authors.call, label: 'Authors'
     end
     f.actions
+  end
+
+  member_action :delete_image, method: :delete do
+    ActiveStorage::Blob.find(params[:id]).delete
+    ActiveStorage::Attachment.find(params[:id]).purge
+    ActiveAdmin::Dependency.rails.redirect_back self, active_admin_root
   end
 end
