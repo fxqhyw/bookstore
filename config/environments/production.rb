ef01785f319adcb53c6454357693081f87622453
therealloker <therealloker@gmail.com>
2018-09-20 16:07:55 +0300

    set env mailer variable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.action_mailer.default_url_options = { host: 'loker-bookstore.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] || 'loker-bookstore.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
